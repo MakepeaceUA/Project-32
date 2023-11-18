edb50253f701fd3a4e41ddd031e263933602b01c
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2023-11-18 14:58:32 +0200

    Add files via upload

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,39 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+
+
+
+void Func(FILE* Text)
+{
+    FILE* C_file = fopen("copytext.txt", "w");
+    int Lines = 1;
+    char str[1000];
+ 
+   
+    while (fgets(str, sizeof(str), Text) != NULL) 
+    {
+        if (Lines % 2 != 0) 
+        {
+            fputs(str, C_file);
+        }
+        Lines++;
+    }
+    
+    fclose(C_file);
+}
+       
+   
+int main() {
+    setlocale(0, "RU");
+    
+    FILE* file = fopen("text.txt", "w");
+    fprintf(file, "Съешь\nже\nещё\nэтих\nмягких\nфранцузских\nбулок,\nда\nвыпей\nчаю\nВедь\nэто\nтак\nвкусно.\nПривет,\nМир!Как\nдела?Что\nты\nделаешь?\nПока,\nпока" );
+    fclose(file);
+    
+    file = fopen("text.txt", "r");
+    Func(file);
+    fclose(file);
+}
